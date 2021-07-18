f2cf941928f79530281908e47044c21c1adfba2d
patrick-moreira <patrickmoreira1995@gmail.com>
2021-07-18 12:27:24 -0300

    Refactoring: Header, Sidebar and Content

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+interface HeaderProps {
+  category: string;
+}
+
+export function Header({ category }: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {category}</span></span>
+    </header>
+  )
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,49 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+import { api } from '../services/api';
+import { GenreResponseProps } from '../App';
+import { Button } from '../components/Button';
+
+interface SideBarProps {
+  selectedGenreId: number;
+  setSelectedGenreId: Function;
+  setSelectedGenre: Function;
+}
+
+export function SideBar({
+  selectedGenreId,
+  setSelectedGenreId,
+  setSelectedGenre
+}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    });
+  }, [selectedGenreId]);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => setSelectedGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
+
 }
